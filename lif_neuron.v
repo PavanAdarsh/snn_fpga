a05030fce5a793b08fa5d1c2a2386225d4a06c27
Pavan <pavanadarsh2019@gmail.com>
2026-05-14 17:56:53 +0530

    first commit, single-layer SNN

---
+++ lif_neuron.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.05.2026 19:39:14
+// Design Name: 
+// Module Name: lif_neuron
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lif_neuron #(parameter DATA_SIZE = 4)
+            (input_spike,weights_flat,clk,reset,threshold,leak,output_spike);
+
+    input [DATA_SIZE-1:0] input_spike;
+    input [(DATA_SIZE*8)-1:0] weights_flat; //32 bit array each time
+    input clk,reset;
+    input [15:0] threshold,leak;
+    
+    output reg output_spike;
+    
+    reg [15:0] membrane;
+    reg [15:0] sum;
+    
+    //weighted sum calculation, purely combinational
+    integer i;
+    always@(*) begin
+        sum = 0;
+        for(i=0;i<DATA_SIZE;i=i+1) begin
+            sum = sum + input_spike[i]*weights_flat[(i*8)+:8];
+        end
+    end
+
+    //lif neuron implementation
+    always@(posedge clk) begin
+        if(reset) begin
+            membrane <= 0;
+            output_spike <= 0;  end
+        else if(membrane >= threshold) begin
+            membrane <= 0;
+            output_spike <= 1; end
+        else begin
+            if(membrane + sum >= leak) membrane <= membrane + sum - leak;
+            else membrane <= membrane + sum;
+            output_spike <= 0;
+        end
+    end
+    
+endmodule
---
+++ network.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.05.2026 11:29:25
+// Design Name: 
+// Module Name: network
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module network(clk,reset,input_spike,output_spike);
+
+    parameter DATA_SIZE = 4, NEURON_NUM = 4;
+    
+    input clk,reset;
+    input [DATA_SIZE-1:0] input_spike;
+    output [NEURON_NUM-1:0] output_spike;
+    
+    reg [15:0] thresholds [NEURON_NUM-1:0];
+    reg [15:0] leaks [NEURON_NUM-1:0];
+    //reg [7:0] weights [NEURON_NUM-1:0][DATA_SIZE-1:0];
+    
+    //4(neuron_num) separate 32(data_size*8)bit registers.
+    //flattening weights to avoid verilog 2-d array issues
+    reg [(DATA_SIZE*8)-1:0] weights_flat [NEURON_NUM-1:0]; 
+     
+    genvar i;
+    generate //to instantiate NEURON_NUM copies of LIF module
+        for(i=0;i<NEURON_NUM;i=i+1)
+        begin : neuron_array
+            lif_neuron #(.DATA_SIZE(DATA_SIZE)) neuron_inst (
+                        .clk(clk),.reset(reset),.threshold(thresholds[i]),.leak(leaks[i]),
+                        .input_spike(input_spike),.output_spike(output_spike[i]),
+                        .weights_flat(weights_flat[i]));
+        end
+    endgenerate
+endmodule
